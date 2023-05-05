f6dbe2e2bf605209712f9ffb2e6bdda65f450881
Igor Alexandria <igorgomes120297@gmail.com>
2023-05-05 13:16:27 -0300

    first commit

---
+++ eventos/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Evento(models.Model):
+    criador = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, blank=True)
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    data_inicio = models.DateField()
+    data_termino = models.DateField()
+    carga_horaria = models.IntegerField()
+    logo = models.ImageField(upload_to='logos')
+    participantes = models.ManyToManyField(User, related_name='evento_participante', null=True, blank=True)
+
+    # Paleta de Cores
+    cor_principal = models.CharField(max_length=7)
+    cor_secundaria = models.CharField(max_length=7)
+    cor_fundo = models.CharField(max_length=7)
+
+    def __str__(self):
+        return self.nome
